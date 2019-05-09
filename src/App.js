99f30d689955063a31e6515fcb67d31e3b56b21d
lemon <925349824@qq.com>
2019-05-09 15:25:35 +0800

    feat:网站头部

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './common/header';
+import { GlobalStyle } from './style';
+
+function App() {
+  return (
+    <div>
+      <GlobalStyle/>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
